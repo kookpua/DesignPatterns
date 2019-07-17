d203ad661828c5a00bc41aaca8e9919b2516314b
gaonan <gaonanlvmm@gmail.com>
2019-07-17 23:27:36 +0800

    add Template Method

---
+++ Polymorphism/Line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class Line:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw Line");
+        }
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libarary libarary = new Application();
+            libarary.Run();
+            Console.ReadKey();
+        }
+    }
+}
