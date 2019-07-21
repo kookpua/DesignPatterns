6b785ea773088d50bc4f2a936db8fd0437dea6bc
gaonan <gaonanlvmm@gmail.com>
2019-07-21 12:36:10 +0800

    add observer/event

--- 李建忠 - C++设计模式_C#代码/Strategy/SalesOrder.cs
+++ 李建忠 - C++设计模式_C#代码/Strategy/SalesOrder.cs
@@ -11,10 +11,10 @@ namespace Strategy
         /// 未完成 等我后边学了工厂模式
         /// </summary>
         /// <param name="strategyFactory"></param>
-        public SalesOrder(StrategyFactory strategyFactory)
-        {
-            strategyFactory = strategyFactory.NewStrategy()
-        }
+        //public SalesOrder(StrategyFactory strategyFactory)
+        //{
+        //    strategyFactory = strategyFactory.NewStrategy()
+        //}
         public double CalculateTax()
         {
             Double context = 0.5;
